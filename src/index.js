b4fab8d8cbc6b4aa36ed285e5730f6083eb7702d
Ocheben <ocheben@gmail.com>
2019-08-08 03:15:22 +0100

    Updated redux devtools

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import reducer from './_store/reducer';
 
 const store = createStore(reducer, compose(
   applyMiddleware(thunk),
-  window.devToolsExtension ? window.devToolsExtension() : f => f
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
 ));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
